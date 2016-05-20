49ae69f5ae6da2bc4de7bb222b972be633b68690
Jaco Greeff <jacogr@gmail.com>
2016-05-20 09:50:40 +0200

    decoder WIP (string)

--- src/decoder/decoder.js
+++ src/decoder/decoder.js
@@ -1,3 +1,5 @@
+import utf8 from 'utf8';
+
 import Token from '../token';
 import BytesTaken from './bytesTaken';
 import DecodeResult from './decodeResult';
@@ -36,47 +38,44 @@ export default class Decoder {
       bytes = `${bytes}${this.peek(slices, position + idx)}`;
     }
 
-    return new BytesTaken(bytes, position + slicesLength);
+    return new BytesTaken(bytes.substr(0, length * 2), position + slicesLength);
   }
 
   decodeParam (param, slices, offset) {
     let taken;
+    let lengthOffset;
+    let length;
 
     switch (param.type) {
       case 'address':
-        return new DecodeResult(
-          new Token(param.type, asAddress(this.peek(slices, offset))),
-          offset + 1
-        );
+        return new DecodeResult(new Token(param.type, asAddress(this.peek(slices, offset))), offset + 1);
 
       case 'bool':
-        return new DecodeResult(
-          new Token(param.type, asBool(this.peek(slices, offset))),
-          offset + 1
-        );
+        return new DecodeResult(new Token(param.type, asBool(this.peek(slices, offset))), offset + 1);
 
       case 'int':
       case 'uint':
-        return new DecodeResult(
-          new Token(param.type, this.peek(slices, offset)),
-          offset + 1
-        );
+        return new DecodeResult(new Token(param.type, this.peek(slices, offset)), offset + 1);
 
       case 'fixedBytes':
         taken = this.takeBytes(slices, offset, param.length);
-        return new DecodeResult(
-          new Token(param.type, taken),
-          taken.newOffset
-        );
+        return new DecodeResult(new Token(param.type, taken), taken.newOffset);
 
       case 'bytes':
-        const lengthOffset = asU32(this.peek(slices, offset)).div(32);
-        const length = asU32(this.peek(slices, lengthOffset));
+        lengthOffset = asU32(this.peek(slices, offset)).div(32);
+        length = asU32(this.peek(slices, lengthOffset));
         taken = this.takeBytes(slices, lengthOffset + 1, length);
-        return new DecodeResult(
-          new Token(param.type, taken.bytes),
-          offset + 1
-        );
+        return new DecodeResult(new Token(param.type, taken.bytes), offset + 1);
+
+      case 'string':
+        lengthOffset = asU32(this.peek(slices, offset)).div(32);
+        length = asU32(this.peek(slices, lengthOffset));
+        taken = this.takeBytes(slices, lengthOffset + 1, length);
+        const str = taken.bytes
+          .match(/.{1,2}/)
+          .map((code) => String.fromCharCode(`0x${code}`))
+          .join('');
+        return new DecodeResult(new Token(param.type, utf8.decode(str)), offset + 1);
 
       default:
         throw new Error(`Invalid param type ${param.type} in decodeParam`);
