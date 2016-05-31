4ae31fa24bae473e2a2876e852895924a86e9bc0
Jaco Greeff <jacogr@gmail.com>
2016-05-31 11:14:07 +0200

    rework order

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,16 +6,16 @@ if [[ ("$TRAVIS_PULL_REQUEST" != "false") || ("$TRAVIS_BRANCH" != "master") ]]; 
   exit 0
 fi
 
-npm run build
-
 git config --global user.email "admin@travis-ci.org"
 git config --global user.name "Travis CI"
 git config --global push.default simple
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GITHUB_TOKEN}:@github.com" > .git/credentials
 
+echo -e "$NPM_USERNAME\n$NPM_PASSWORD\n$NPM_EMAIL" | npm login
+
 git checkout master
+npm run build
 git add --force ./index.js
 
-echo -e "$NPM_USERNAME\n$NPM_PASSWORD\n$NPM_EMAIL" | npm login
 echo -e "" | node_modules/.bin/release-it
