acdbb0240236470f0c71ce71d8d446b055ee7f9b
Jaco Greeff <jacogr@gmail.com>
2016-05-31 11:33:59 +0200

    try to hunt down index.js

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,15 +6,15 @@ if [[ ("$TRAVIS_PULL_REQUEST" != "false") || ("$TRAVIS_BRANCH" != "master") ]]; 
   exit 0
 fi
 
-npm run build
-
 git config --global user.email "admin@travis-ci.org"
 git config --global user.name "Travis CI"
 git config --global push.default simple
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GITHUB_TOKEN}:@github.com" > .git/credentials
-
 git checkout master
+
+npm run build
+ls -al ./index.js
 git add --force ./index.js
 
 node_modules/.bin/release-it
