e4b36e73dd31b68b268c738ccbf63be2e8c23b1a
Jaco Greeff <jacogr@gmail.com>
2016-05-31 07:39:13 +0200

    rollup library creation

---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+import babel from 'rollup-plugin-babel';
+
+export default {
+  entry: 'lib/ethAbi.js',
+  dest: 'index.js',
+  moduleName: 'EthAbi',
+  format: 'cjs',
+  plugins: [babel({
+    babelrc: false,
+    presets: ['es2015-rollup', 'stage-0'],
+    runtimeHelpers: true,
+    retainLines: true
+  })]
+};
