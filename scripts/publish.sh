4cd6226f2ffb1dc1c9930598bf1487e001c9d850
Jaco Greeff <jacogr@gmail.com>
2016-06-01 10:50:23 +0200

    attempt publish to npm registry

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -13,12 +13,17 @@ git config credential.helper "store --file=.git/credentials"
 echo "https://${GITHUB_TOKEN}:@github.com" > .git/credentials
 git checkout master
 
-npm run build
 DATE=`date`
+
+npm run build
 echo "/* ${DATE} */" >> ./index.js
+
 git add --force index.js
 git commit --message "[CI skip] ${DATE}"
 
-npm version patch --message "[CI skip] ${DATE} %s"
+VERSION=`npm version patch --message "[CI skip] ${DATE} %s" | sed s/v//`
 git push
 git push --tags
+
+echo -e "$NPM_USERNAME\n$NPM_PASSWORD\n$NPM_EMAIL" | npm login
+npm publish --tag $VERSION
