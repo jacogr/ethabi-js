a90b407a70ae1bb54864fb602b5c0aef501a2f62
Travis CI <admin@travis-ci.org>
2016-06-01 10:47:37 +0000

    [CI skip] Wed Jun  1 10:47:34 UTC 2016

--- index.js
+++ index.js
@@ -653,13 +653,30 @@ EventParam = function () {
     params) {
       return params.map(function (param) {return new EventParam(param.name, param.type, param.indexed);});} }]);return EventParam;}();
 
+// eslint-disable-line camelcase
+
+function eventSignature(name, params) {
+  var types = (params || []).map(function (input) {return input.type;}).join(',');
+  var id = (name || '') + '(' + types + ')';
+
+  return jsSha3.keccak_256(id);}
+
+
+function methodSignature(name, params) {
+  return eventSignature(name, params).substr(0, 8);}
+
 var 
 
 Event = function () {
   function Event(abi) {babelHelpers.classCallCheck(this, Event);
     this._name = abi.name;
     this._inputs = EventParam.toEventParams(abi.inputs || []);
-    this._anonymous = !!abi.anonymous;}babelHelpers.createClass(Event, [{ key: 'inputParamTypes', value: function inputParamTypes() 
+    this._anonymous = !!abi.anonymous;
+    this._signature = eventSignature(this._name, this.inputParamTypes());}babelHelpers.createClass(Event, [{ key: 'inputParamTypes', value: function inputParamTypes() 
+
+
+
+
 
 
 
@@ -724,15 +741,7 @@ Event = function () {
       var decodedParams = this.inputParamNames().
       map(function (name, idx) {return new DecodedLogParam(name, inputParamTypes[idx], namedTokens[name]);});
 
-      return new DecodedLog(decodedParams, address);} }, { key: 'name', get: function get() {return this._name;} }, { key: 'inputs', get: function get() {return this._inputs;} }, { key: 'anonymous', get: function get() {return this._anonymous;} }]);return Event;}();
-
-// eslint-disable-line camelcase
-
-function signature(name, params) {
-  var types = (params || []).map(function (input) {return input.type;}).join(',');
-  var id = (name || '') + '(' + types + ')';
-
-  return jsSha3.keccak_256(id).substr(0, 8);}
+      return new DecodedLog(decodedParams, address);} }, { key: 'name', get: function get() {return this._name;} }, { key: 'inputs', get: function get() {return this._inputs;} }, { key: 'anonymous', get: function get() {return this._anonymous;} }, { key: 'signature', get: function get() {return this._signature;} }]);return Event;}();
 
 var 
 
@@ -740,7 +749,12 @@ Func = function () {
   function Func(abi) {babelHelpers.classCallCheck(this, Func);
     this._name = abi.name;
     this._inputs = Param.toParams(abi.inputs || []);
-    this._outputs = Param.toParams(abi.outputs || []);}babelHelpers.createClass(Func, [{ key: 'inputParamTypes', value: function inputParamTypes() 
+    this._outputs = Param.toParams(abi.outputs || []);
+    this._signature = methodSignature(this._name, this.inputParamTypes());}babelHelpers.createClass(Func, [{ key: 'inputParamTypes', value: function inputParamTypes() 
+
+
+
+
 
 
 
@@ -764,14 +778,11 @@ Func = function () {
 
 
     tokens) {
-      var signed = signature(this.name, this.inputParamTypes());
-      var encoded = Encoder.encode(tokens);
-
-      return '' + signed + encoded;} }, { key: 'decodeOutput', value: function decodeOutput(
+      return '' + this._signature + Encoder.encode(tokens);} }, { key: 'decodeOutput', value: function decodeOutput(
 
 
     data) {
-      return Decoder.decode(this.outputParamTypes(), data);} }, { key: 'name', get: function get() {return this._name;} }, { key: 'inputs', get: function get() {return this._inputs;} }, { key: 'outputs', get: function get() {return this._outputs;} }]);return Func;}();
+      return Decoder.decode(this.outputParamTypes(), data);} }, { key: 'name', get: function get() {return this._name;} }, { key: 'inputs', get: function get() {return this._inputs;} }, { key: 'outputs', get: function get() {return this._outputs;} }, { key: 'signature', get: function get() {return this._signature;} }]);return Func;}();
 
 var 
 
@@ -827,4 +838,4 @@ var
 
 EthAbi = function (_Interface) {babelHelpers.inherits(EthAbi, _Interface);function EthAbi() {babelHelpers.classCallCheck(this, EthAbi);return babelHelpers.possibleConstructorReturn(this, Object.getPrototypeOf(EthAbi).apply(this, arguments));}return EthAbi;}(Interface);
 
-module.exports = EthAbi;/* Wed Jun  1 09:01:19 UTC 2016 */
+module.exports = EthAbi;/* Wed Jun  1 10:47:34 UTC 2016 */
