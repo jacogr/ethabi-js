ef515814238cc5ba5bd12a7c7ab2dbd7918b36ed
Travis CI <admin@travis-ci.org>
2016-06-09 05:36:23 +0000

    [CI skip] Thu Jun  9 05:36:20 UTC 2016

--- index.js
+++ index.js
@@ -1082,4 +1082,4 @@ var EthAbi = function (_Interface) {
   return EthAbi;
 }(Interface);
 
-module.exports = EthAbi;/* Wed Jun  8 21:18:04 UTC 2016 */
+module.exports = EthAbi;/* Thu Jun  9 05:36:20 UTC 2016 */
